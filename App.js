f70031e5e0fa8271035fe6de8d9310b4b7aaa5f7
Ritik <rdsharma3883@gmail.com>
2019-09-09 20:31:09 +0530

    try 2 for pull option

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
+        <Text>Hello, world! Hello Guys</Text>
       </View>
     );
   }
