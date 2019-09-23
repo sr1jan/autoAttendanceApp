f45e0426366f71c08aa993de4dee583a4da074c9
sr1jan <srofficialsingh@gmail.com>
2019-09-23 23:24:59 +0530

    Integrated firebase storage to upload images

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { Text, View, Image, Button, StyleSheet} from 'react-native';
+import { View, Image, Button, StyleSheet, Alert } from 'react-native';
 
 import ImagePicker from 'react-native-image-picker';
+import firebase from 'react-native-firebase';
+
+const { app } = firebase.storage();
+console.log(app);
 
 export default class App extends Component {
-
   state = {
     photo: null,
   };
@@ -12,32 +15,46 @@ export default class App extends Component {
   handleChoosePhoto = () => {
     const options = {
       noData: true,
-      permissionDenied: true,
     };
+
     ImagePicker.showImagePicker(options, response => {
-      console.log('response', response);
       if (response.uri) {
         this.setState({ photo: response });
-        console.log('photo --> ', this.state);
+
+        const image = {
+          path : response.uri.toString(),
+        };
+        this.uploadImage(image, 'Test Image')
+          .then(() => {
+              Alert.alert('Uploaded', 'Image successfully uploaded to the firebase database');
+          })
+          .catch((error) => {
+              console.log(error.message);
+              Alert.alert('Error', 'Something went wrong');
+          });
       }
     });
-  };
+  }
+
+  uploadImage = async (image, imageName) => {
+    return firebase.storage().ref('images/' + imageName).putFile(image.path);    
+  }
 
   render() {
     const { photo } = this.state;
     return (
-      <View style={{ flex: 1, justifyContent: 'space-evenly', }}>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-evenly' }}>
+      <View style={styles.main}>
+        <View style={styles.container}>
           <Image
-            style={styles.stretch}
             source={require('./src/images/logo.png')}
+            style={styles.stretch}
           />
-         <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
+         <Button title="Choose Photo" color='black' onPress={this.handleChoosePhoto} />
         </View>
-        <View style={{ padding: 20, justifyContent: 'flex-end', alignItems: 'center' }}>
-         {photo && (
+        <View style={styles.img_container}>
+          {photo && (
             <Image source={{ uri: photo.uri }} style={styles.userImg} />
-         )}
+          )}
         </View>
       </View>
     );
@@ -45,11 +62,19 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: '#4c8bf5',
+  },
+
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+
   stretch: {
-    width : '100%',
-    height: '10%',
-    resizeMode: 'contain',
-    position: 'relative',
+    resizeMode: 'center',
   },
 
   title: {
@@ -62,6 +87,12 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
     position: 'relative',
+  },
+
+  img_container: {
+    padding: 20,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
 
 });
--- android/app/src/main/java/com/firstproject/MainApplication.java
+++ android/app/src/main/java/com/firstproject/MainApplication.java
@@ -7,9 +7,12 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 
 import java.util.List;
 
@@ -27,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseStoragePackage());
       return packages;
     }
 
